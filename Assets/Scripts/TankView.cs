b246602e67e56185384e8ba220d3929e234ae1c0
Ashwin02 <ashwinabraham2001@gmail.com>
2025-04-29 15:06:21 +0530

    Added tank movement + camera view

--- Assets/Scripts/TankView.cs
+++ Assets/Scripts/TankView.cs
@@ -1,25 +1,46 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class TankView : MonoBehaviour
 {
     private TankController tankController;
+
+    private float movement;
+    private float rotation;
+
+    public Rigidbody rb;
     
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameObject cam = GameObject.Find("Main Camera");
+        cam.transform.SetParent(transform);
+        cam.transform.position = new Vector3(0f, 3f, -4f);
     }
+    public Rigidbody GetRigidbody() { return rb; }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
+
+        if (movement != 0)
+            tankController.Move(movement, tankController.GetTankModel().movementSpeed);
+
+        if (rotation != 0)
+            tankController.Rotate(rotation, tankController.GetTankModel().rotationSpeed);
     }
 
     public void SetTankController(TankController _tankController)
     {
         tankController = _tankController;
+    }
+
+    private void Movement()
+    {
+        movement = Input.GetAxis("Vertical");
+        rotation = Input.GetAxis("Horizontal");
     }
 }
